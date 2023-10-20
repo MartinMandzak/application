4271a55e10adfb84202677088401307edb487116
MartinMandzak <102599044+MartinMandzak@users.noreply.github.com>
2023-10-20 23:42:01 +0200

    I'm an idiot

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(){
 
 	std::cout<<"First string of the application!"<<std::endl;
-	std::cout<<"First second of the application!"<<std::endl;
+	std::cout<<"Second string of the application!"<<std::endl;
 	return 0;  
 
 }

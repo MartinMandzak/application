d3269b64be304eb6d409788e288c47f9f9a36ba3
MartinMandzak <102599044+MartinMandzak@users.noreply.github.com>
2023-10-21 03:20:03 +0200

    islands

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <random>
 
+std::vector<bool> board(100,false);
+std::random_device rd;
 
+void draw_board(){	
+	for(int idx=0;idx<board.size();idx++){
+		if(idx%10 == 0){std::cout<<std::endl;}
+		if(board[idx]){
+			std::cout<<" O ";
+		}else{
+			std::cout<<" - ";
+		}
+	}
+}
 
+void generate_islands(){
+	std::uniform_int_distribution<int> island_chance (0,9);
+	std::cout<<island_chance(rd);
+}
 
 int main(){
+	
+	generate_islands();
+	draw_board();
 
-	std::cout<<"First string of the application!"<<std::endl;
-	std::cout<<"Second string of the application!"<<std::endl;
 	return 0;  
 
 }

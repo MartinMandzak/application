68505cc3d08c474f2cf39f9e7b421d93d5e4eebf
MartinMandzak <mandzak.martin3@gmail.com>
2023-10-23 08:29:05 +0200

    idk wtf i was doing here, simply do not ask

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int SIZE = 100;
 std::vector<bool> board(SIZE,false);
 //std::random_device rd; - slow-updating seed
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); //quick-updating seed
-std::mt19937 random_gen(seed);
+std::mt19937 random_gen(seed); //pseudo random system using a random seed	
+std::uniform_int_distribution<int> island_chance(0,9); //range dist, could be float by using "real"
 
 void draw_board(){	
 	for(int idx=0;idx<SIZE;idx++){
@@ -21,7 +22,8 @@ void draw_board(){
 }
 
 void generate_islands(){
-	std::uniform_int_distribution<int> island_chance(0,9);
+
+
 	for(int idx = 0;idx<SIZE;idx++){
 		int chance = island_chance(random_gen);
 		if(chance ==9){
@@ -30,11 +32,20 @@ void generate_islands(){
 	}
 }
 
+void game_of_life(int n_of_iterations){
+	for(int n = 0;n < n_of_iterations;n++){
+		generate_islands();
+		draw_board();
+		std::cout<<"\n\n\n";
+	}
+}
+
 int main(){
+
+	int n = 10;
 	
 	generate_islands();
 	draw_board();
 
 	return 0;  
-
 }
